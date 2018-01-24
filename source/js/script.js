24b2340770fc44942a270d19e997d2242b944594
Elena Esina <hellemess@yandex.ru>
2018-01-24 19:20:32 +0300

    оформила блок с отзывами, настроила переключение между отзывами взрослых и школьников, вынесла форму в отдельный блок, чтобы оформить ее как всплывающее окно

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ const ENTER = 13;
 
 const isActivationEvent = (evt) => evt.keyCode && evt.keyCode === ENTER;
 
+// открытие и закрытие меню сайта по нажанию на иконку меню
+
 const menu = document.querySelector('.header__nav');
 const menuToggle = menu.querySelector('.header__menu-toggle');
 const menuList = menu.querySelector('.header__menu-list');
@@ -18,4 +20,18 @@ menuToggle.addEventListener('keydown', (evt) => {
   if (isActivationEvent(evt)) {
     toggleMenu;
   }
+});
+
+// переключение между отзывами взрослых и школьников
+
+const feedback = document.querySelector('.feedback');
+const feedbackToggle = feedback.querySelector('.feedback__toggle');
+const feedbackSelections = feedback.querySelectorAll('.feedback__selection');
+
+feedback.classList.remove('feedback--no-js');
+
+feedbackToggle.addEventListener('change', () => {
+  feedbackSelections.forEach((it) => {
+    it.classList.toggle('feedback__selection--show');
+  });
 });
